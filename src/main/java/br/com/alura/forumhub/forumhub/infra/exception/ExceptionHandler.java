0deffb3ae465faa6fcb22615a2ef3601b805cf29
Robson Moura <robsonmoura970@gmail.com>
2024-07-02 21:18:45 -0300

    Feat: Adicionando as classes de Log e de tratamento de exceptions no projeto

---
+++ src/main/java/br/com/alura/forumhub/forumhub/infra/exception/ExceptionHandler.java
@@ -0,0 +1,61 @@
+package br.com.alura.forumhub.forumhub.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity <String> tratarElementoNaoEncontradoNoBd () {
+        return new ResponseEntity<>("Recurso não encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity <String> tratarEntidadeNaoEncontrada () {
+        return new ResponseEntity<>("Entidade não encontrada", HttpStatus.NOT_FOUND);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(
+                FieldError::getRejectedValue)
+        );
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+//
+//    @org.springframework.web.bind.annotation.ExceptionHandler(BadCredentialsException.class)
+//    public ResponseEntity tratarErroBadCredentials() {
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+//    }
+//
+//    @org.springframework.web.bind.annotation.ExceptionHandler(AuthenticationException.class)
+//    public ResponseEntity tratarErroAuthentication() {
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+//    }
+//
+//    @org.springframework.web.bind.annotation.ExceptionHandler(AccessDeniedException.class)
+//    public ResponseEntity tratarErroAcessoNegado() {
+//        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+//    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
+
+
+
+}
